f658e25558fc54267c6b0c0189d42f615e01c797
Arthur Coelho <arthur.cueio@gmail.com>
2019-04-09 18:16:11 -0300

    Updates

---
+++ precache-manifest.875d17c47337855b387ead30b290153b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c82f2325acccdd379e66",
+    "url": "/markdown-previewer/static/css/main.ce8481a4.chunk.css"
+  },
+  {
+    "revision": "c82f2325acccdd379e66",
+    "url": "/markdown-previewer/static/js/main.f15f0e8c.chunk.js"
+  },
+  {
+    "revision": "d1c7fdd19b32b1a54c00",
+    "url": "/markdown-previewer/static/js/runtime~main.398dfc58.js"
+  },
+  {
+    "revision": "aa5aad70ccbb537b34df",
+    "url": "/markdown-previewer/static/js/2.29106b03.chunk.js"
+  },
+  {
+    "revision": "8a850f9b65da0daa4a7500ae97a82e13",
+    "url": "/markdown-previewer/index.html"
+  }
+];
